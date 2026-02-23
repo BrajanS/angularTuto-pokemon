ce08c9cbed3bdb4278e0a810475e8cb38f221c9f
BrajanS <szczapbrajan33@gmail.com>
2026-02-23 20:19:29 +0100

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemons";
+import { PokemonInterface } from "./pokemonData";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["../styles.css"],
 })
-export class AppComponent {
-  title = 'ng-first-project-app';
+export class AppComponent implements OnInit {
+  // prettier-ignore
+  pokemonDatas = POKEMONS.map((p, incrementedID) => ({ ...p, _uid: incrementedID }));
+  pokemonStuff: PokemonInterface | undefined;
+
+  ngOnInit(): void {
+    this.getPokemonLog(POKEMONS);
+  }
+
+  getPokemonLog(pokemonArr: PokemonInterface[]) {
+    if (typeof pokemonArr === "object" && pokemonArr !== null) {
+      console.table(pokemonArr);
+      /*pokemonArr.forEach((elpokemon: PokemonInterface) => {
+        console.info("chosen Pokemon:", elpokemon.name);
+      });*/
+    }
+  }
 }
---
+++ src/app/mock-pokemons.ts
@@ -0,0 +1,124 @@
+import { PokemonInterface } from "./pokemonData";
+
+export const POKEMONS: PokemonInterface[] = [
+  {
+    id: 1,
+    hp: 45,
+    cp: 39,
+    name: "Bulbasaur",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    types: ["Grass", "Poison"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 4,
+    hp: 39,
+    cp: 52,
+    name: "Charmander",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    types: ["Fire"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 7,
+    hp: 44,
+    cp: 48,
+    name: "Squirtle",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    types: ["Water"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 25,
+    hp: 35,
+    cp: 112,
+    name: "Pikachu",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    types: ["Electric"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 39,
+    hp: 115,
+    cp: 40,
+    name: "Jigglypuff",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/39.png",
+    types: ["Normal", "Fairy"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 54,
+    hp: 40,
+    cp: 50,
+    name: "Psyduck",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/54.png",
+    types: ["Water"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 63,
+    hp: 25,
+    cp: 20,
+    name: "Abra",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/63.png",
+    types: ["Psychic"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 74,
+    hp: 40,
+    cp: 53,
+    name: "Geodude",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/74.png",
+    types: ["Rock", "Ground"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 92,
+    hp: 30,
+    cp: 35,
+    name: "Gastly",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/92.png",
+    types: ["Ghost", "Poison"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 104,
+    hp: 50,
+    cp: 50,
+    name: "Cubone",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/104.png",
+    types: ["Ground"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 133,
+    hp: 55,
+    cp: 55,
+    name: "Eevee",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/133.png",
+    types: ["Normal"],
+    created: new Date("1996-04-27"),
+  },
+  {
+    id: 147,
+    hp: 41,
+    cp: 60,
+    name: "Dratini",
+    picture:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/147.png",
+    types: ["Dragon"],
+    created: new Date("1996-04-27"),
+  },
+];
